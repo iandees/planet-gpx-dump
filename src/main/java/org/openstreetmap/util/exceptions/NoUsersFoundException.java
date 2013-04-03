73257d6fe1f996511bfc7ef273c2b6720c9eaf8f
Ian Dees <ian.dees@gmail.com>
2013-04-03 08:55:01 -0500

    Remove Java stuff for now.

--- src/main/java/org/openstreetmap/util/exceptions/NoUsersFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.openstreetmap.util.exceptions;
-
-public class NoUsersFoundException extends DatabaseException {
-    public NoUsersFoundException() {
-        super("No users could be found. Something is wrong! (i.e. MAX(id) doesn't return a result)");
-    }
-}
